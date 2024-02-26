894f2803dc23cfb72aaa154dc623f6b688a0a798
shubhFreelance <shubham58ch@gmail.com>
2024-02-27 01:00:21 +0530

    quiz app MVP-done

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -5,15 +5,24 @@ import styles from "./page.module.css";
 import { data } from "@/data";
 
 export default function Quiz() {
-  const [index, setIndex] = useState(3);
+  let [index, setIndex] = useState(0);
   const [question, setQuestion] = useState(data[index]);
   const [selectedChoice, setSelectedChoice] = useState(null);
 
+  //handling selected answer
   const handleChoiceClick = (choice) => {
-    
     setSelectedChoice(choice);
   };
- console.log(selectedChoice)
+
+//function for handling next button
+  const handleNext=()=>{
+    console.log("first")
+    setIndex(++index)
+    setQuestion(data[index])
+  }
+
+
+  console.log(selectedChoice);
   return (
     <div className={styles.main}>
       <h1 className={styles.h1}>Quiz time</h1>
@@ -22,39 +31,53 @@ export default function Quiz() {
 
         <div className={styles.question}>
           <h2 className={styles.questionTitle}>
-            {index + 1}. 
-            <span>{question.question}</span>
+            {index + 1}.<span>{question.question}</span>
           </h2>
           <ul className={styles.ul}>
             <li
-              className={`${styles.li} ${selectedChoice === question.choice1 ? styles.selected : ""}`}
+              className={`${styles.li} ${
+                selectedChoice === question.choice1 ? styles.selected : ""
+              }`}
               onClick={() => handleChoiceClick(question.choice1)}
             >
               {question.choice1}
             </li>
             <li
-              className={`${styles.li} ${selectedChoice === question.choice2 ? styles.selected : ""}`}
+              className={`${styles.li} ${
+                selectedChoice === question.choice2 ? styles.selected : ""
+              }`}
               onClick={() => handleChoiceClick(question.choice2)}
             >
               {question.choice2}
             </li>
             <li
-              className={`${styles.li} ${selectedChoice === question.choice3 ? styles.selected : ""}`}
+              className={`${styles.li} ${
+                selectedChoice === question.choice3 ? styles.selected : ""
+              }`}
               onClick={() => handleChoiceClick(question.choice3)}
             >
               {question.choice3}
             </li>
             <li
-              className={`${styles.li} ${selectedChoice === question.choice4 ? styles.selected : ""}`}
+              className={`${styles.li} ${
+                selectedChoice === question.choice4 ? styles.selected : ""
+              }`}
               onClick={() => handleChoiceClick(question.choice4)}
             >
               {question.choice4}
             </li>
+            
           </ul>
         </div>
+        <div className={styles.end}>
         <p className={styles.qNumber}>
-          1 <span>/5</span>
+          {index+1} <span>/{data.length}</span>
         </p>
+
+
+        <button className={`${styles.nextButton} ${selectedChoice === null ? styles.nextDisabled : ""}`} disabled={selectedChoice===null} onClick={handleNext}>Next</button>
+        
+        </div>
       </div>
     </div>
   );
