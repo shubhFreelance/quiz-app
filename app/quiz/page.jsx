2daea3c91f9ce9ef2047741de131f802a460003b
shubhFreelance <shubham58ch@gmail.com>
2024-02-26 23:47:54 +0530

    basic setup done

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,17 +1,39 @@
+"use client";
+
 import { useState } from "react";
 import styles from "./page.module.css";
+import { data } from "@/data";
 
-export default function Quiz(){
+export default function Quiz() {
+  const [index, setIndex] = useState(3);
+  const [question, setQuestion] = useState(data[index])
 
+  
 
-    return (
-        <div className={styles.main}>
-            <h1>Quiz time</h1>
-            <div className={styles.card}>
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.h1}>Quiz time</h1>
+      <div className={styles.card}>
+        <h1 className={styles.cardTitle}>Question {index +1}</h1>
 
-                
-            </div>
+        <div className={styles.question}>
+          <h2 className={styles.questionTitle}>
             
+          {index +1}. 
+            <span>{question.question}</span>
+          </h2>
+          <ul className={styles.ul}>
+            <li className={styles.li}>{question.choice1}</li>
+            <li className={styles.li}>{question.choice2}</li>
+            <li className={styles.li}>{question.choice3}</li>
+            <li className={styles.li}>{question.choice4}</li>
+            
+          </ul>
         </div>
-    )
+        <p className={styles.qNumber}>
+          1 <span>/5</span>
+        </p>
+      </div>
+    </div>
+  );
 }
--- data.js
+++ data.js
@@ -1,58 +1,58 @@
 export const data =[
     {
       "question": "How would you describe your previous experience with investing?",
-      "option1": "Beginner",
-      "option2": "Intermediate",
-      "option3": "Advanced",
-      "option4": "I have not invested before"
+      "choice1": "Beginner",
+      "choice2": "Intermediate",
+      "choice3": "Advanced",
+      "choice4": "I have not invested before"
     },
     {
       "question": "In general, how would you characterize your tolerance for investment risk?",
-      "option1": "Low (Prefer conservative and stable investments)",
-      "option2": "Moderate (Balanced approach with some risk)",
-      "option3": "High (Comfortable with higher-risk investments)",
-      "option4": "I'm not sure"
+      "choice1": "Low (Prefer conservative and stable investments)",
+      "choice2": "Moderate (Balanced approach with some risk)",
+      "choice3": "High (Comfortable with higher-risk investments)",
+      "choice4": "I'm not sure"
     },
     {
       "question": "What is your primary financial goal for investing?",
-      "option1": "Capital appreciation (Seeking growth in investment value)",
-      "option2": "Income generation (Seeking regular dividends or interest)",
-      "option3": "Diversification (Spreading risk across different assets)",
-      "option4": "Other (Please specify)"
+      "choice1": "Capital appreciation (Seeking growth in investment value)",
+      "choice2": "Income generation (Seeking regular dividends or interest)",
+      "choice3": "Diversification (Spreading risk across different assets)",
+      "choice4": "Other (Please specify)"
     },
     {
       "question": "What is your preferred investment time horizon?",
-      "option1": "Short-term (1-3 years)",
-      "option2": "Medium-term (3-5 years)",
-      "option3": "Long-term (5+ years)",
-      "option4": "It depends on the opportunity"
+      "choice1": "Short-term (1-3 years)",
+      "choice2": "Medium-term (3-5 years)",
+      "choice3": "Long-term (5+ years)",
+      "choice4": "It depends on the opportunity"
     },
     {
       "question": "How important is diversification in your investment strategy?",
-      "option1": "Very important (I prefer a well-diversified portfolio)",
-      "option2": "Somewhat important (I consider diversification but not overly strict)",
-      "option3": "Not important (I focus on a few select opportunities)",
-      "option4": "I'm not sure"
+      "choice1": "Very important (I prefer a well-diversified portfolio)",
+      "choice2": "Somewhat important (I consider diversification but not overly strict)",
+      "choice3": "Not important (I focus on a few select opportunities)",
+      "choice4": "I'm not sure"
     },
     {
       "question": "Which industry or sector interests you the most for investment?",
-      "option1": "Technology",
-      "option2": "Healthcare",
-      "option3": "Finance",
-      "option4": "Other (Please specify)"
+      "choice1": "Technology",
+      "choice2": "Healthcare",
+      "choice3": "Finance",
+      "choice4": "Other (Please specify)"
     },
     {
       "question": "What is your preferred exit strategy for investments?",
-      "option1": "Selling to other investors",
-      "option2": "Holding until IPO (Initial Public Offering)",
-      "option3": "Acquisition by another company",
-      "option4": "Other (Please specify)"
+      "choice1": "Selling to other investors",
+      "choice2": "Holding until IPO (Initial Public Offering)",
+      "choice3": "Acquisition by another company",
+      "choice4": "Other (Please specify)"
     },
     {
       "question": "How often do you stay informed about market trends and economic indicators?",
-      "option1": "Daily",
-      "option2": "Weekly",
-      "option3": "Monthly",
-      "option4": "Rarely"
+      "choice1": "Daily",
+      "choice2": "Weekly",
+      "choice3": "Monthly",
+      "choice4": "Rarely"
     }
   ]
